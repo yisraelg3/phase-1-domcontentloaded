d00a114f49f5841972a0196e8b1ec1620fd7be94
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-02 14:43:29 -0400

    commit tree 4c0d91f3aa71d72c6a687e9f073d43f83fe19cf1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const text = document.getElementById('text')
+    text.textContent = 'This is really cool!'
+})
+
